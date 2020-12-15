4a1894dc55445861af467ae245c85d0e773942b8
ProgrammedRookie <18604319768@163.com>
2020-12-15 21:56:21 +0800

    计算器实现

--- src/main/java/com/zy/stack/ArrayOrNodeStack.java
+++ src/main/java/com/zy/stack/ArrayOrNodeStack.java
@@ -8,15 +8,59 @@ import javax.management.relation.RoleUnresolved;
 public class ArrayOrNodeStack {
 
     public static void main(String[] args) {
-        NodeStack ns = new NodeStack();
-        ns.push( 1);
-        ns.push( 2);
-        ns.push(3);
-        ns.push( 4);
 
-        ns.list();
+        int index = 0;
+        int num1 = 0;
+        int num2 = 0;
+        int res = 0;
+        int ch = ' ';
+        int oper = 0;
 
-        System.out.println(ns.pop());
+        String es = "30+9*2-2";
+
+        ArrayStack numsStack = new ArrayStack(10);
+
+        ArrayStack operStack = new ArrayStack(10);
+
+        while (true){
+
+            ch = es.substring(index,index+1).charAt(0);
+
+            if (operStack.isOper(ch)){
+                if (operStack.isEmpty()){
+                    operStack.push(ch);
+                }else {
+                    if (operStack.priority(ch) <= operStack.priority(operStack.peek())){
+                        num1 = numsStack.pop();
+                        num2 = numsStack.pop();
+                        oper = operStack.pop();
+                        res = numsStack.cal(num1,num2,oper);
+                        numsStack.push(res);
+                        operStack.push(ch);
+                    }else {
+                        operStack.push(ch);
+                    }
+                }
+            }else {
+                // todo 将数放入数字栈
+                numsStack.push(ch -48);
+            }
+            index++;
+            if (index == es.length()){
+                break;
+            }
+        }
+
+        while (true){
+            if (operStack.isEmpty()){
+                break;
+            }
+            num1 = numsStack.pop();
+            num2 = numsStack.pop();
+            res = numsStack.cal(num1,num2,operStack.pop());
+            numsStack.push(res);
+        }
+        System.out.println(numsStack.pop());
     }
 }
 
@@ -136,4 +180,56 @@ class ArrayStack {
             System.out.printf("栈内index[%d]====  栈内元素%d \n",i,stack[i]);
         }
     }
+
+    /**
+     * 判断符号优先级
+     * @param oper
+     * @return
+     */
+    public int priority(int oper){
+        if (oper == '*' || oper == '/'){
+            return 1;
+        }else if (oper == '+' || oper == '-'){
+            return 0;
+        }else {
+            return -1;
+        }
+    }
+
+    /**
+     * 计算
+     * @param num1
+     * @param num2
+     * @param oper
+     * @return
+     */
+    public int cal(int num1,int num2,int oper){
+        int res = 0;
+        switch (oper){
+            case '+':
+                res = num1 + num2;
+                break;
+            case '-':
+                res = num2 - num1;
+                break;
+            case '*':
+                res = num1 * num2;
+                break;
+            case '/':
+                res = num2 / num1;
+                break;
+                default:break;
+        }
+        return res;
+    }
+
+    public int peek(){
+        return stack[top];
+    }
+
+    public boolean isOper(int oper){
+        return oper == '*' || oper == '+' || oper == '-' || oper == '/';
+    }
+
+
 }
